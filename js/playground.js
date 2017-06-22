addbd636c1f6d76287209a945ad7ad5fe9e7d1af
hmhm903 <hmhm903m@gmail.com>
2017-06-22 18:02:57 +0900

    Check the support state of  File API.

--- js/playground.js
+++ js/playground.js
@@ -1,1 +1,6 @@
-console.log("Hello world")
+// Check for the various File API support.
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+  // Great success! All the File APIs are supported.
+} else {
+  alert('The File APIs are not fully supported in this browser.');
+}
