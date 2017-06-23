4979f72668e6557a4e1fdffe684164f6cbf0d624
hmhm903 <hmhm903m@gmail.com>
2017-06-23 18:11:06 +0900

    Test select files and drag and drop files on browser.

--- js/playground.js
+++ js/playground.js
@@ -4,3 +4,41 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
 } else {
   alert('The File APIs are not fully supported in this browser.');
 }
+
+function handleFileSelect1(evt)
+{
+  var files = evt.target.files;
+
+  for(var i =0, f; f = files[i]; i++)
+  {
+    console.log(f);
+  }
+}
+
+function handleFileSelect2(evt)
+{
+  evt.stopPropagation();
+  evt.preventDefault();
+  var files = evt.dataTransfer.files;
+
+  for(var i =0, f; f = files[i]; i++)
+  {
+    console.log(f);
+  }
+
+}
+
+function handleDragOver(evt)
+{
+  evt.stopPropagation();
+  evt.preventDefault();
+  evt.dataTransfer.dropEffect = 'copy';
+}
+
+//select file
+document.getElementById('files').addEventListener('change', handleFileSelect1, false);
+
+//drag and drop
+var drop_zone = document.getElementById('drop_zone');
+drop_zone.addEventListener('dragover', handleDragOver, false);
+drop_zone.addEventListener('drop', handleFileSelect2, false);
